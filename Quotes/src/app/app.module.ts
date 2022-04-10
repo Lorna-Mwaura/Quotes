2de85ab9b999ba74ffaad708c7b471e4f34e5e01
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-10 21:20:04 +0300

    generated media component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MediaComponent } from './media/media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MediaComponent
   ],
   imports: [
     BrowserModule,

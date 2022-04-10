66d9816b038481e69c54ff6db1f28a177a2dc31b
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-10 21:34:04 +0300

    generated quote component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MediaComponent } from './media/media.component';
 import { HomeComponent } from './home/home.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     MediaComponent,
-    HomeComponent
+    HomeComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,

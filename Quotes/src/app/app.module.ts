3ec142a156be023b7a9e38ce717cccfc01bf8376
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-10 21:27:50 +0300

    generated the home component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MediaComponent } from './media/media.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MediaComponent
+    MediaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

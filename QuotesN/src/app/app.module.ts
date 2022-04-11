62dda0b185315127f9563ac2a9ecdd75a927f7af
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-12 01:51:03 +0300

    generated the background component

--- QuotesN/src/app/app.module.ts
+++ QuotesN/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 import { TimeElapsedPipe } from './time-elapsed.pipe';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { BackgroundComponent } from './background/background.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { QuoteDetailsComponent } from './quote-details/quote-details.component';
     HighlightDirective,
     FormComponent,
     TimeElapsedPipe,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
